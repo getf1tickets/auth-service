e4a9cb2b871aaff20b87bcfca9dcd83295306a77
iverly <iverly.dev@gmail.com>
2022-01-08 12:01:57 +0100

    feat: refactor sdk with plugins

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { join } from 'path';
 import AutoLoad, { AutoloadPluginOptions } from 'fastify-autoload';
 import { FastifyPluginAsync } from 'fastify';
-import { registerPlugins } from '@getf1tickets/sdk';
 
 export type AppOptions = {} & Partial<AutoloadPluginOptions>;
 
@@ -9,14 +8,10 @@ const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
   opts,
 ): Promise<void> => {
-  // register sdk plugins
-  await registerPlugins(fastify);
-
-  // no custom plugin for now
-  // void fastify.register(AutoLoad, {
-  //   dir: join(__dirname, 'plugins'),
-  //   options: opts
-  // })
+  fastify.register(AutoLoad, {
+    dir: join(__dirname, 'plugins'),
+    options: opts,
+  });
 
   fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
