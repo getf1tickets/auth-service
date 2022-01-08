cb253f809a25679526bfdbc4d6735da1131388ed
iverly <iverly.dev@gmail.com>
2022-01-08 10:21:32 +0100

    feat: add eslint

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
 import { join } from 'path';
-import AutoLoad, {AutoloadPluginOptions} from 'fastify-autoload';
+import AutoLoad, { AutoloadPluginOptions } from 'fastify-autoload';
 import { FastifyPluginAsync } from 'fastify';
 import { registerPlugins } from '@getf1tickets/sdk';
 
 export type AppOptions = {} & Partial<AutoloadPluginOptions>;
 
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+  fastify,
+  opts,
 ): Promise<void> => {
   // register sdk plugins
-  await registerPlugins(fastify)
-  
+  await registerPlugins(fastify);
+
   // no custom plugin for now
   // void fastify.register(AutoLoad, {
   //   dir: join(__dirname, 'plugins'),
   //   options: opts
   // })
 
-  void fastify.register(AutoLoad, {
+  fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
-    options: opts
-  })
+    options: opts,
+  });
 };
 
 export default app;
-export { app }
+export { app };
